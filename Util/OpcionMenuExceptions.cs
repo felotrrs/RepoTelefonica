5183f8b767ddf7ad69303f4f2d9e89b08c237d08
felotrrs <felotrrs@users.noreply.github.com>
2019-03-16 01:41:13 -0600

    Corrections

--- Util/OpcionMenuExceptions.cs
+++ Util/OpcionMenuExceptions.cs
@@ -2,11 +2,10 @@ using System;
 namespace CentralTelefonica.Util
 
 {
-    public class OpcionMenuException : Exception
-    {
+    public class OpcionMenuException : Exception {
         private string message = "Error,  debe ingresar un numero,  no un caracter";
-        public override string message {
-            get {return message;}
+        public override string Message {
+            get { return message; }
         }
     }
 }

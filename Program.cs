f8ec993c5bd5f2b33ce4722ec261c57fe07f54a5
Felipe Torres <felotrrs@gmail.com>
2019-03-16 08:42:02 -0600

    actualizacion
    actualizacion

--- Program.cs
+++ Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using CentralTelefonica.Entidades;
 using CentralTelefonica.App;
+using CentralTelefonica.Util;
 namespace CentralTelefonica
 {
     class Program
     {
         static void Main(string[] args)
         {
-           MenuPrincipal menu = new MenuPrincipal();
-           menu.MostrarMenu();
+            try
+            {
+                MenuPrincipal menu = new MenuPrincipal();
+                menu.MostrarMenu();
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+           
         }
     }
 }
